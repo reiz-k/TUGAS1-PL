da2cea62c93e60d2b57c8de80d35f50dcba3fbb7
Fauzan Rizaldi <12450111056@students.uin-suska.ac.id>
2025-02-25 21:29:59 +0700

    Add files via upload

---
+++ BacaCSV.java
@@ -0,0 +1,71 @@
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class BacaCSV {
+    public static void main(String[] args) {
+        String csvFile = "laptops_train.csv";
+        DataLaptop[] ListLaptop = new DataLaptop[978]; //data laptop sebanyak 978
+
+        try (BufferedReader br = new BufferedReader(new FileReader(csvFile))){
+
+            String line = "";
+            br.readLine(); //skip baris pertama
+            int idx = 0;
+
+            // membaca setiap baris dalam file CSV
+            while ((line = br.readLine()) != null){
+                String[] file = line.split(",(?=([^\"]*\"[^\"]*\")*[^\"]*$)");
+
+                // membuat objek DataCSV dan mengisi data
+                DataLaptop dataLaptop = new DataLaptop();
+                dataLaptop.Manufacturer = file[0];
+                dataLaptop.ModelName = file[1];
+                dataLaptop.Category = file[2];
+                dataLaptop.ScreenSize = file[3];
+                dataLaptop.Screen = file[4];
+                dataLaptop.CPU = file[5];
+                dataLaptop.RAM = file[6];
+                dataLaptop.Storage = file[7];
+                dataLaptop.GPU = file[8];
+                dataLaptop.OperatingSystem = file[9];
+                dataLaptop.OperatingSystemVersion = file[10];
+                dataLaptop.Weight = file[11];
+                dataLaptop.Price = file[12];
+
+
+                ListLaptop [idx] = dataLaptop;
+                idx++;
+
+                dataLaptop.print();
+
+
+                 // memanggil metode print untuk menampilkan informasi laptop
+
+
+            }
+            System.out.println("===========================");
+            System.out.println("");
+            System.out.println("Jenis laptop dengan spesifik : ");
+            br.close();
+
+        } catch (IOException e) {
+            System.out.println("Error : " + e.getMessage());
+        }
+
+        //pencarian untuk semua laptop yg diproduksi oleh "Apple"
+
+        for (DataLaptop dataLaptop : ListLaptop) {
+
+            if (dataLaptop !=null && dataLaptop.Manufacturer.equalsIgnoreCase("Apple")) {
+                dataLaptop.print();
+            }
+
+
+        }
+
+        System.out.println("");
+
+    }
+}
---
+++ DataLaptop.java
@@ -0,0 +1,22 @@
+//deklarasi variabel informasi pada laptop
+public class DataLaptop {
+        public String Manufacturer;
+        public String ModelName;
+        public String Category;
+        public String ScreenSize;
+        public String Screen;
+        public String CPU;
+        public String RAM;
+        public String Storage;
+        public String GPU;
+        public String OperatingSystem;
+        public String OperatingSystemVersion;
+        public String Weight;
+        public String Price;
+
+        //menampilkan informasi tertentu yang ingin ditampilkan
+        public void print(){
+                System.out.println("Manufacturer : " + Manufacturer + ", Model: " + ModelName + ", CPU : " + CPU + ", RAM: " + RAM + ", Harga: " + Price);
+
+    }
+}
